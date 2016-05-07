895a8037986baefe6176819011b78c5a9cdca8cd
Andrew Dourgarian <andrewdourgarian@yahoo.com>
2016-05-07 14:20:27 -0500

    router fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var moodCol = require('../models/moods.js');
-var randomNumber = Math.floor(Math.random() * 6);
 
 /* GET home page. */
 router.get('/moodOutput', function(req, res, next) {
@@ -12,6 +11,7 @@ router.get('/moodOutput', function(req, res, next) {
 });
 
 router.post('/moodInput', function(req, res, next){
+  console.log("asdflkjsdf");
   console.log(req.body);
 
   res.send("You've successfully posted to the server", req.body);
